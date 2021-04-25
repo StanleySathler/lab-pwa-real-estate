839824e1af50602f5f10a15209a50de2f779bba9
Stanley Sathler <stanleysathlerpinto@gmail.com>
2021-04-25 20:37:29 -0300

    Setup Netlify & Heroku deployment (#2)
    * change manifest colors
    
    * use env var for api url
    
    * prepare for heroku deploy
    
    * use port from env
    
    * add express as prod dependency

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const mock = [
   { name: "Apartamento em Santo Agostinho", price: 300000 },
