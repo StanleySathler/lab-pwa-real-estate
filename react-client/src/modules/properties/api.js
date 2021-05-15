e2fb23d33a0fd481281cffa9dc3df3b7e30fc670
StanleySathler <stanleysathlerpinto@gmail.com>
2021-05-15 12:52:02 -0300

    Remove React app
    This will be a Vanilla app from now on.

--- react-client/src/modules/properties/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const baseUrl = `${process.env.REACT_APP_API_URL}/properties`;
-
-export const findAll = async () => {
-  const { data } = await axios.get(baseUrl);
-  return data;
-};
