7a720efc6e4702fc9ff0e99a691cc6180b36bc9c
StanleySathler <stanleysathlerpinto@gmail.com>
2021-05-30 20:29:38 -0300

    fix header

--- .github/actions/deploy-heroku-ra/index.js
+++ .github/actions/deploy-heroku-ra/index.js
@@ -11,18 +11,20 @@ const storage = require("./storage");
 const PIPELINE_ID = "6f0589a9-927b-4d01-add3-642b8fdb479a";
 const AUTH_TOKEN = "aa9af670-a72c-4898-9e53-df0a4e1cec0b";
 
-const baseHeaders = {
-  Authorization: `Bearer ${AUTH_TOKEN}`,
+const defaultOptions = {
+  headers: {
+    Authorization: `Bearer ${AUTH_TOKEN}`,
+  },
 };
 
 const listReviewApps = async () =>
-  client.get(`/pipelines/${PIPELINE_ID}/review-apps`, baseHeaders);
+  client.get(`/pipelines/${PIPELINE_ID}/review-apps`, defaultOptions);
 
 const deleteReviewApp = async (app) =>
-  client.delete(`/review-apps/${app.id}`, baseHeaders);
+  client.delete(`/review-apps/${app.id}`, defaultOptions);
 
 const createReviewApp = async (sourceUrl) =>
-  client.post("/review-apps", baseHeaders, {
+  client.post("/review-apps", defaultOptions, {
     body: {
       branch: process.env.GITHUB_REF_SLUG, // todo: use env
       pipeline: PIPELINE_ID, // todo: move to secrets
