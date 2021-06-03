071a044ca49fcceec9a0777656a52cddb15565c2
StanleySathler <stanleysathlerpinto@gmail.com>
2021-06-03 19:14:36 -0300

    fix branch name

--- .github/actions/deploy-heroku-ra/dist/index.js
+++ .github/actions/deploy-heroku-ra/dist/index.js
@@ -69172,6 +69172,7 @@ const storage = __nccwpck_require__(9960);
  */
 const PIPELINE_ID = "6f0589a9-927b-4d01-add3-642b8fdb479a";
 const AUTH_TOKEN = "aa9af670-a72c-4898-9e53-df0a4e1cec0b";
+const BRANCH_NAME = process.env.GITHUB_HEAD_REF_SLUG;
 
 const defaultOptions = {
   headers: {
@@ -69201,7 +69202,7 @@ const createReviewApp = async (sourceUrl) =>
   client.post("/review-apps", {
     ...defaultOptions,
     body: {
-      branch: process.env.GITHUB_HEAD_REF_SLUG, // todo: use env
+      branch: BRANCH_NAME, // todo: use env
       pipeline: PIPELINE_ID, // todo: move to secrets
       pr_number: Number(core.getInput("pr-number")),
       source_blob: { url: sourceUrl, version: "0.0.1" },
@@ -69211,7 +69212,7 @@ const createReviewApp = async (sourceUrl) =>
 const findByBranch = (apps, branch) => apps.find((a) => a.branch === branch);
 
 const createApp = async (sourceUrl) => {
-  const currentBranch = process.env.GITHUB_REF_SLUG;
+  const currentBranch = BRANCH_NAME;
   const apps = await listReviewApps();
   const app = findByBranch(apps, currentBranch);
 
--- .github/actions/deploy-heroku-ra/index.js
+++ .github/actions/deploy-heroku-ra/index.js
@@ -10,6 +10,7 @@ const storage = require("./storage");
  */
 const PIPELINE_ID = "6f0589a9-927b-4d01-add3-642b8fdb479a";
 const AUTH_TOKEN = "aa9af670-a72c-4898-9e53-df0a4e1cec0b";
+const BRANCH_NAME = process.env.GITHUB_HEAD_REF_SLUG;
 
 const defaultOptions = {
   headers: {
@@ -39,7 +40,7 @@ const createReviewApp = async (sourceUrl) =>
   client.post("/review-apps", {
     ...defaultOptions,
     body: {
-      branch: process.env.GITHUB_HEAD_REF_SLUG, // todo: use env
+      branch: BRANCH_NAME, // todo: use env
       pipeline: PIPELINE_ID, // todo: move to secrets
       pr_number: Number(core.getInput("pr-number")),
       source_blob: { url: sourceUrl, version: "0.0.1" },
@@ -49,7 +50,7 @@ const createReviewApp = async (sourceUrl) =>
 const findByBranch = (apps, branch) => apps.find((a) => a.branch === branch);
 
 const createApp = async (sourceUrl) => {
-  const currentBranch = process.env.GITHUB_REF_SLUG;
+  const currentBranch = BRANCH_NAME;
   const apps = await listReviewApps();
   const app = findByBranch(apps, currentBranch);
 
